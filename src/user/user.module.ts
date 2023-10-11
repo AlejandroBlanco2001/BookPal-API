89272130e1b17613657ebc22a250dd69f335f9de
Isaac Blanco <nuclearhands2001@gmail.com>
2023-10-10 21:33:44 -0500

    Prisma moduled in the User Module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
+  imports: [PrismaModule],
 })
 export class UserModule {}

092e6f9fd00d3c152a9acd74265c54b9070210bc
Isaac Blanco <nuclearhands2001@gmail.com>
2023-11-08 21:43:06 -0500

    Change schema

---
+++ prisma/migrations/20231109023608_firebase_default_value_change/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "PhysicalBook" ALTER COLUMN "book_cover" SET DEFAULT '/no_cover.jpg';
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "profile_image" SET DEFAULT '/default_logo.jpg';

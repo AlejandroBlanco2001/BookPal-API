ce4c4a808a0377552df6021a0ac361993f190705
Isaac Blanco <40709873+AlejandroBlanco2001@users.noreply.github.com>
2023-12-01 12:03:50 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
     .setTitle('Book Pal')
